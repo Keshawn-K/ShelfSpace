1e311b4c4fa1ceb95b72d5339f3f804eba2a8276
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-07-23 21:10:57 +0300

    fix: allow all CORS origins temporarily

--- backend/shelfspace/settings.py
+++ backend/shelfspace/settings.py
@@ -102,11 +102,7 @@ SIMPLE_JWT = {
 }
 
 # CORS
-CORS_ALLOWED_ORIGINS = [
-    'https://shelfspace-0bxe.onrender.com',
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # drf-spectacular
