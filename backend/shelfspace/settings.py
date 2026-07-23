ec650d070ce5ce5b927eec5ba796aa28b89570bc
Keshawn-K <keshawn.kingori@student.moringaschool.com>
2026-07-23 21:03:22 +0300

    added frontend URL to CORS allowed origins

--- backend/shelfspace/settings.py
+++ backend/shelfspace/settings.py
@@ -103,9 +103,11 @@ SIMPLE_JWT = {
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    'https://shelfspace.onrender.com',
+    'https://shelfspace-0bxe.onrender.com',
     'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
+
 
 # drf-spectacular
 SPECTACULAR_SETTINGS = {
